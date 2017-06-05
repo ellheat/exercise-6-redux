1bda8f72a0c7902f6b0665fd140730a4e20e6996
Marcin Bazanowski <mbazanowski@apptension.com>
2017-06-05 12:11:19 +0200

    current team logic added

--- app/modules/reducers.js
+++ app/modules/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux-immutable';
 
 import routerReducer from './router/router.redux';
 import teamsReducer from './teams/teams.reducer';
+import teamReducer from './team/team.reducer';
 
 
 export default function createReducer() {
   return combineReducers({
     route: routerReducer,
     teams: teamsReducer,
+    team: teamReducer,
   });
 }
---
+++ app/modules/team/team.reducer.js
@@ -0,0 +1,23 @@
+import { fromJS, Record, List } from 'immutable';
+import { createReducer } from 'reduxsauce';
+
+import { teamActionsTypes } from './team.actions';
+
+const StateRecord = new Record({
+  name: null,
+  title: null,
+  images: List(),
+});
+
+const INITIAL_STATE = new StateRecord();
+
+const changeTeam = (state = INITIAL_STATE, action) => state.merge({
+  name: action.payload.name,
+  title: action.payload.title,
+});
+
+const HANDLERS = {
+  [teamActionsTypes.CHANGE_TEAM]: changeTeam,
+};
+
+export default createReducer(INITIAL_STATE, HANDLERS);
