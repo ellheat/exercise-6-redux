66dbee4e7c8b072e5e4270eefdd00fd47fcdc712
Marcin Bazanowski <mbazanowski@apptension.com>
2017-06-06 09:36:27 +0200

    old tests fixed

--- app/routes/__tests__/app.container.spec.js
+++ app/routes/__tests__/app.container.spec.js
@@ -6,12 +6,6 @@ import { mapDispatchToProps } from '../app.container';
 
 describe('App: Container', () => {
   describe('mapDispatchToProps', () => {
-    it('should call LocalesActions.setLanguage', () => {
-      const dispatch = spy();
 
-      mapDispatchToProps(dispatch).setLanguage();
-
-      expect(dispatch.firstCall.args[0]).to.deep.equal(LocalesActions.setLanguage());
-    });
   });
 });
