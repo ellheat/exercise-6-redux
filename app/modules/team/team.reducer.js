c0815e4a6f2fdd53cdf32f2b5d382a4466c86f4f
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2017-06-07 19:45:06 +0200

    fix some reducers

--- app/modules/team/team.reducer.js
+++ app/modules/team/team.reducer.js
@@ -10,13 +10,11 @@ const StateRecord = new Record({
 
 const INITIAL_STATE = new StateRecord();
 
-const changeTeam = (state = INITIAL_STATE, action) => state.merge({
+const changeTeam = (state, action) => state.merge({
   name: action.payload.name,
   title: action.payload.title,
 });
 
-const HANDLERS = {
+export default createReducer(INITIAL_STATE, {
   [teamActionsTypes.CHANGE_TEAM]: changeTeam,
-};
-
-export default createReducer(INITIAL_STATE, HANDLERS);
+});
